822c3c54769ac6fc6c078efc9a3d8e11ce97aba2
Manish Kumar Ray <manishkrrish84@gmail.com>
2020-12-23 18:49:34 +0530

    first commit

---
+++ speech assistant.py
@@ -0,0 +1,64 @@
+import speech_recognition as sr
+from time import ctime
+import time
+import webbrowser
+import playsound
+import os
+import random
+from gtts import gTTS
+
+r=sr.Recognizer()
+def record_audio(ask=False):
+    with sr.Microphone() as source:
+
+        if ask:
+            chomu_speak(ask)
+        audio=r.listen(source)
+        voice_data=""
+        try:
+            voice_data=r.recognize_google(audio)
+        except sr.UnknownValueError:
+            chomu_speak("Sorry, I didn't get that")
+        except sr.RequestError:
+            chomu_speak("Sorry, My speech service is down")
+        return voice_data
+
+def chomu_speak(audio_string):
+    tts=gTTS(text=audio_string,lang='en')
+    r=random.randint(1,10000000)
+    audio_file='audio-'+str(r)+'.mp3'
+    tts.save(audio_file)
+    playsound.playsound(audio_file)
+    print(audio_string)
+    os.remove(audio_file)
+
+def respond(voice_date):
+    if "hi" in voice_data:
+        chomu_speak("How can I help you ?")
+    if "what is your name" in voice_data:
+        chomu_speak("My name is Chikni chameli")
+    if "what time is it" in voice_data:
+        chomu_speak(ctime())
+    if "search" in voice_data:
+        search=record_audio("what do you want to search for?")
+        url="https://google.com/search?q="+search
+        webbrowser.get().open(url)
+        chomu_speak("here is what i found for "+search)
+    if "find location" in voice_data:
+        location=record_audio("what is your location?")
+        url="https://google.nl/maps/place/"+location+"/&amp"
+        webbrowser.get().open(url)
+        chomu_speak("here is the location of "+location)
+    if 'bye' in voice_data:
+        chomu_speak("bye, take care of yourself")
+        exit()
+    else:
+        chomu_speak("How can I help you ?")
+
+
+
+time.sleep(1)
+chomu_speak("hey")
+while 1:
+    voice_data=record_audio()
+    respond(voice_data)
